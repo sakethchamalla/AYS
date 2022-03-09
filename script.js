e90858ebc65fc13107b878e390d68f72e67e2477
Prabhash Varma <varmaprabhash@gmail.com>
2022-03-09 14:33:11 +0530

    update

---
+++ script.js
@@ -0,0 +1,12 @@
+
+$(function(){
+
+        $(window).scroll(function() {
+            if( $(this).scrollTop() > 50 ) {
+                $('.navbar').addClass('solid bg-dark');
+            }
+            else{
+                $('.navbar').removeClass('solid bg-dark');
+            }
+        })
+})
